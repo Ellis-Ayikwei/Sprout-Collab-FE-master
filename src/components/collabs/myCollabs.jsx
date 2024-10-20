6c6064c1d461ec45583854c6c9bb09e0d7f085bc
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-10-20 09:25:26 +0000

    Refactor MyCollabs component to use DotLoader for loading animation

--- src/components/collabs/myCollabs.jsx
+++ src/components/collabs/myCollabs.jsx
@@ -1,8 +1,8 @@
+import DotLoader from "components/DotLoader";
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 import fetcher from "../../helpers/fetcher";
 import Handshake from "../../images/collabb.png";
-import Loader from "../Loader";
 import GenericCard from "../task/genericCard";
 
 const MyCollabs = () => {
@@ -23,9 +23,8 @@ const MyCollabs = () => {
 
 	return (
 		<div className="h-full">
-			{isLoading && <Loader />}
-
 			<div className="mt-6 flex flex-col items-center justify-center h-full w-full p-2 gap-2">
+				{isLoading && <DotLoader />}
 				{collabs &&
 					collabs.slice(0, 3).map((collab) => (
 						<GenericCard
